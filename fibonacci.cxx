de34094b4de5b5ebcc5b832ff5a20dc556a8b380
Janina Müller <janina@Janinas-MBP.fritz.box>
2015-11-01 15:45:43 +0100

    Homework1

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,24 @@
+// fibonacci Reihe
 
+#include <iostream>
+using namespace std;
+int main(){
+    int a=0;
+    int b=1;
+    int c;
+    int N;
+    
+    //Einlesen von N
+    cout << "N = ?" << endl;
+    cin >> N;
+    
+    // for-Schleife zur Berechnung der N-ten Fibonaccizahl
+    for(int i=2; i<N+1; i++){
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    // Ausgabe der N-ten Fibonaccizahl
+    cout << c << endl;
+    return 0;
+}
